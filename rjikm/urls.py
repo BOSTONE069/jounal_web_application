28f31d36586f6498457e7ab33dba1ba50c1c382c
BOSTONE069 <bostoneochieng69@gmail.com>
2022-12-20 12:33:16 -0500

    created the about page

--- rjikm/urls.py
+++ rjikm/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("about", views.about, name="about")
 ]
