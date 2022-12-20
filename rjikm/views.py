a244762dd663f301f94afa432c191aa4d91f8d1c
rhemney <bostoneochieng69@gmail.com>
2022-12-20 04:32:26 -0800

    addjusted the different section of the web app

--- rjikm/views.py
+++ rjikm/views.py
@@ -5,3 +5,18 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "rjikm/index.html")
+
+def about(request):
+    return render(request, "rjikm/about.html")
+
+def contact(request):
+    return render(request, "rjikm/contact.html")
+
+def articles(request):
+    return render(request, "rjikm/articles.html")
+
+def editorials(request):
+    return render(request, "rjikm/editorials.html")
+
+def submit(request):
+    return render(request, "rjikm/submit.html")
