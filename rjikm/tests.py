fa75f1f56efe8edcd7495238fe847f3caf3e63aa
rhemney <bostoneochieng69@gmail.com>
2022-12-21 14:46:59 -0800

    added the different test cases for the models

--- rjikm/tests.py
+++ rjikm/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Editorinchief
+# It creates an Editorinchief object with the given attributes.
+class EditorinchiefTestCase(TestCase):
+    def setUp(self):
+        Editorinchief.objects.create(
+            FullName='Tom', University='Technical University of Kenya', MyEmail='tom.kwanya@gmail.com'
+        )
+
+
+def test_str_method(self):
+  """
+  The function tests the str method of the Editorinchief model
+  """
+  editorinchief = Editorinchief.objects.get(pk=1)
+  self.assertEqual(str(editorinchief),
+                     'Tom Technical University of Kenya tom.kwanya@gmail.com')
+
+
+def test_save_method(self):
+  """
+  It tests the save method of the Editorinchief model.
+  """
+  editorinchief = Editorinchief.objects.get(pk=1)
+  self.assertEqual(editorinchief.FullName, 'Tom')
+  self.assertEqual(editorinchief.University, 'Technical University of Kenya')
+  self.assertEqual(editorinchief.MyEmail, 'tom.kwanya@gmail.com')
