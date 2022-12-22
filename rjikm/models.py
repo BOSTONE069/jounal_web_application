91fffa78432459fec2d115c6003324673927077b
BOSTONE069 <bostoneochieng69@gmail.com>
2022-12-21 22:11:06 -0500

    adjusted the models file

--- rjikm/models.py
+++ rjikm/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 # It creates a class called ListItem that has two fields: item and order.
@@ -8,12 +9,12 @@ from django.db import models
 class Editorialboard(models.Model):
     Prefix = models.CharField(max_length=20, default="Prof")
     FirstName = models.CharField(max_length=100, default='Tom')
-    SecondName = models.CharField(max_length=100, default= 'Kwanya')
+    SecondName = models.CharField(max_length=100, default='Kwanya')
     University = models.CharField(max_length=100, default="Technical University of Kenya")
     Country = models.CharField(max_length=100, default="Kenya")
 
     def __str__(self):
-      return f"{self.Prefix} {self.FirstName} {self.SecondName} {self.University} {self.Country}"
+        return f"{self.Prefix} {self.FirstName} {self.SecondName} {self.University} {self.Country}"
 
 
 # The class Editorinchief is a model that has three fields: FullName, University and Email
@@ -24,4 +25,4 @@ class Editorinchief(models.Model):
     MyEmail = models.CharField(max_length=100, default="tom.kwanya@gmail.com")
 
     def __str__(self):
-      return f" {self.FullName} {self.University} {self.MyEmail}"
+        return f" {self.FullName} {self.University} {self.MyEmail}"
