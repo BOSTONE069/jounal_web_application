7d0f14f2f43125e408668398e5de9207dac3d41d
rhemney <bostoneochieng69@gmail.com>
2022-12-27 11:22:30 -0800

    Wrote the test cases for the article model.

--- rjikm/tests.py
+++ rjikm/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
-from .models import Editorinchief, Editorialboard
+from .models import Editorinchief, Editorialboard, Article
 
 
 # It creates an Editorinchief object with the given attributes.
@@ -63,3 +63,57 @@ def test_get_method(self):
     self.assertEqual(editorialboard.University,
                      'Technical University of Kenya')
     self.assertEqual(editorialboard.Country, 'Kenya')
+
+
+def test_create_article_with_defaults(self):
+    article = Article.objects.create()
+    self.assertEqual(article.volume, "untitled")
+    self.assertEqual(article.title, "Untitled")
+    self.assertEqual(article.author1, "Anonymous")
+    self.assertEqual(article.university1, "Unknown")
+    self.assertEqual(article.department1, "Unknown")
+    self.assertEqual(article.email1, "anonymous@example.com")
+    self.assertEqual(article.author2, "Anonymous")
+    self.assertEqual(article.department2, "Unknown")
+    self.assertEqual(article.university2, "Unknown")
+    self.assertEqual(article.email2, "anonymous@example.com")
+    self.assertEqual(article.abstract_title, "Abstract")
+    self.assertEqual(article.abstract, "No abstract provided")
+    self.assertEqual(article.keyword_title, "Keyword")
+    self.assertEqual(article.keywords, "None")
+    self.assertEqual(article.titlelink, "Untitled")
+
+
+def test_create_article_with_non_defaults(self):
+    article = Article.objects.create(
+        volume="Volume 1",
+        title="My Article",
+        author1="John Doe",
+        university1="University of Example",
+        department1="Department of Example",
+        email1="john.doe@example.com",
+        author2="Jane Doe",
+        department2="Department of Example",
+        university2="University of Example",
+        email2="jane.doe@example.com",
+        abstract_title="Abstract Title",
+        abstract="This is the abstract of my article.",
+        keyword_title="Keyword Title",
+        keywords="keyword1, keyword2",
+        titlelink="My Article Link"
+    )
+    self.assertEqual(article.volume, "Volume 1")
+    self.assertEqual(article.title, "My Article")
+    self.assertEqual(article.author1, "John Doe")
+    self.assertEqual(article.university1, "University of Example")
+    self.assertEqual(article.department1, "Department of Example")
+    self.assertEqual(article.email1, "john.doe@example.com")
+    self.assertEqual(article.author2, "Jane Doe")
+    self.assertEqual(article.department2, "Department of Example")
+    self.assertEqual(article.university2, "University of Example")
+    self.assertEqual(article.email2, "jane.doe@example.com")
+    self.assertEqual(article.abstract_title, "Abstract Title")
+    self.assertEqual(article.abstract, "This is the abstract of my article.")
+    self.assertEqual(article.keyword_title, "Keyword Title")
+    self.assertEqual(article.keywords, "keyword1, keyword2")
+    self.assertEqual(article.titlelink, "My Article Link")
