07c3984617abf2b99005fb14b87fbe8b740311b0
rhemney <bostoneochieng69@gmail.com>
2023-01-06 11:54:03 -0800

    included the media root into setting of the application to enable dowload of the pdf

--- rjikm_app/settings.py
+++ rjikm_app/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = str(BASE_DIR) + "/uploads/"
+
+MEDIA_URL = '/uploads/'
