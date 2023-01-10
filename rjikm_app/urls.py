dc5e5ea0dff25d1c02df99f11cee89169b590e5b
rhemney <bostoneochieng69@gmail.com>
2023-01-09 17:01:12 -0800

    Did an update to the various files in the program

--- rjikm_app/urls.py
+++ rjikm_app/urls.py
@@ -21,5 +21,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('rjikm/', include("rjikm.urls"))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('rjikm/', include("rjikm.urls")),
+]
