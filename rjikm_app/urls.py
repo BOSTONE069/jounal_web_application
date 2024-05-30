5e8db133800b0444edff0b8553d19d13cf9fea7b
BOSTONE069 <bostoneochieng69@gmail.com>
2024-05-30 13:26:42 +0300

    Updated the urls

--- rjikm_app/urls.py
+++ rjikm_app/urls.py
@@ -21,5 +21,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('rjikm/', include("rjikm.urls")),
+    path('', include("rjikm.urls")),
 ]
